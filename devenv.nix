507ed573b722568c4e6ae6b94f4c37a7f7761d81
Hamza Zahid <hamza.ukw@gmail.com>
2026-03-12 13:31:26 +0100

    Update secretspec command to run quietly

--- devenv.nix
+++ devenv.nix
@@ -105,7 +105,7 @@ in
     '';
     lxai_training.exec = "           
     source .devenv/state/venv/bin/activate
-    secretspec run --provider env uv run python run_training.py";
+    secretspec run --provider env --quiet uv run python run_training.py";
 
     pyshell.exec = "uv run python manage.py shell";
 
